3bc256f7c3dea4c18392da053e9bcfa24f3da3d8
jaseel <jaseelerayassan@gmail.com>
2023-08-14 18:10:36 +0530

    small changes

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from './src/components/HomeScreen'
 import LoginActivity from './src/screens/LoginActivity'
-import LoginOtp from './src/screens/LoginOtp'
 import CheckOutScreen from './src/components/CheckOutScreen'
 
 export default function App() {
@@ -15,7 +14,6 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='LoginActivity' component={LoginActivity} options={{ headerShown: false }} />
-        <Stack.Screen name='LoginOtp' component={LoginOtp} options={{ headerShown: false }} />
         <Stack.Screen name='HomeScreen' component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name='CheckOutScreen' component={CheckOutScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
--- src/screens/LoginActivity.js
+++ src/screens/LoginActivity.js
@@ -1,19 +1,17 @@
 import React, { useState, useRef } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native'
 import PhoneInput from "react-native-phone-number-input";
 import Toast from "react-native-simple-toast";
 import auth from '@react-native-firebase/auth';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-
-
 export default function LoginActivity({ navigation, route }) {
 
-    const [phoneNumber, setPhoneNumber] = useState('')
-    const [country, setCountry] = useState('+91')
+    const [number, setNumber] = useState('')
     const [confirm, setConfirm] = useState('')
-
+    const [code, setCode] = useState('')
     const phoneInput = useRef(null);
+    const [country, setCountry] = useState('+91')
 
     // phone number authentication
 
@@ -22,16 +20,41 @@ export default function LoginActivity({ navigation, route }) {
             const confirmation = await auth().signInWithPhoneNumber(number)
             if (confirmation.state != 'error') {
                 Toast.show('OTP Sented')
-                navigation.navigate('LoginOtp', { confirm: confirmation });
             }
+            setConfirm(confirmation)
         } catch (error) {
-            console.log(error,'mobile');
+            console.log(error, 'mobile');
             Toast.show('Failed to Sent OTP')
         }
     }
 
     const getOTP = () => {
-        signInWithPhoneNumber('+' + country + phoneNumber)
+        signInWithPhoneNumber(country + number)
+    }
+
+    async function confirmCode() {
+        try {
+            await confirm.confirm(code).then(() => {
+                Toast.show('Login successfull')
+                navigation.navigate('HomeScreen')
+                setConfirm(confirm)
+            }).catch(() => {
+                Toast.show('Please Check Your OTP')
+            })
+        } catch (error) {
+            console.log(error, 'login');
+            Toast.show('Failed To Verify OTP')
+        }
+    }
+
+    const onSubmit = () => {
+        if (code.length === 6) {
+            confirmCode()
+        } else if (code.length === 0) {
+            Toast.show('OTP cannot be empty', Toast.SHORT);
+        } else {
+            Toast.show('OTP cannot be empty', Toast.SHORT);
+        }
     }
 
     return (
@@ -45,12 +68,12 @@ export default function LoginActivity({ navigation, route }) {
             <View>
                 <PhoneInput
                     ref={phoneInput}
-                    defaultValue={phoneNumber}
+                    defaultValue={number}
                     defaultCode="IN"
                     codeTextStyle={{ fontSize: hp('1.75%'), marginBottom: hp(0.2) }}
                     textInputStyle={{ fontSize: hp('1.75%'), }}
                     textContainerStyle={{ borderRadius: 10, paddingVertical: 0, backgroundColor: '#fff', height: hp('5.9') }}
-                    onChangeText={text => setPhoneNumber(text)}
+                    onChangeText={text => setNumber(text)}
                     containerStyle={{ height: hp('6'), width: wp('90'), borderWidth: wp('.1'), borderColor: 'gray', borderRadius: hp('1'), margin: hp('1.5'), textAlign: 'center' }}
                     onChangeCountry={text => {
                         setCountry(text.callingCode.join(','))
@@ -63,8 +86,26 @@ export default function LoginActivity({ navigation, route }) {
                     <Text style={{ color: 'white', fontWeight: 'bold', fontSize: hp('2'), textAlign: 'center' }}>GET OTP</Text>
                 </TouchableOpacity>
             </View>
+            <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: hp('5') }}>
+                <Text style={{ color: 'black', fontWeight: 'bold', fontSize: hp('2') }}>Enter OTP</Text>
+            </View>
             <View>
-                <TouchableOpacity onPress={() => navigation.navigate('LoginOtp')}
+                <TextInput
+                    value={code}
+                    onChangeText={text => setCode(text)}
+                    secureTextEntry={true}
+                    inputMode='numeric'
+                    style={{ height: hp('5'), width: wp('90'), borderWidth: wp('.1'), borderColor: 'gray', borderRadius: hp('1'), margin: hp('1.5'), textAlign: 'center' }}
+                />
+            </View>
+            <View>
+                <TouchableOpacity onPress={() => onSubmit()}
+                    style={{ height: hp('5'), width: wp('90'), backgroundColor: 'green', justifyContent: 'center', borderRadius: wp('2.5') }}>
+                    <Text style={{ color: 'white', fontWeight: 'bold', fontSize: hp('2'), textAlign: 'center' }}>SUBMIT</Text>
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}
                     style={{ height: hp('5'), width: wp('20'), backgroundColor: 'white', justifyContent: 'center', borderRadius: wp('2.5') }}>
                     <Text style={{ color: 'black', fontWeight: 'bold', fontSize: hp('2'), textAlign: 'center' }}>SKIP</Text>
                 </TouchableOpacity>
